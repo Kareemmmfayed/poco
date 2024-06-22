83d948d79e3fd3b818d281e06a1510e04b7127a2
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2024-06-22 07:22:39 +0200

    Set up app
    - Installed the Next project
    
     Set up the folder structure
    
    Added the favicon & title

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - Poco Restaurant",
+    default: "Home - Poco Restaurant",
+  },
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
